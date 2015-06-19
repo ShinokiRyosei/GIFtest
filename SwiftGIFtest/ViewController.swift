3f65716ffb26d127efd0ede3e1a676f9eaaaab89
Life is Tech <lifeistech@Life-no-MacBook-Air-14.local>
2015-06-19 15:26:26 +0800

    hello

--- SwiftGIFtest/ViewController.swift
+++ SwiftGIFtest/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var ImageGIF: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib
+        var strImg : String = "http://33.media.tumblr.com/0493c9fd0153d514fd3273507e44b68e/tumblr_mj7lwsQZYb1rmtxh8o1_500.gif"
+        
+        var url: NSURL = NSURL(string: strImg)!
+        
+        ImageGIF.image = UIImage.animatedImageWithAnimatedGIFURL(url)
     }
 
     override func didReceiveMemoryWarning() {
